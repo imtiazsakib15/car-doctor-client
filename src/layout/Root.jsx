ee308524eb2efff7105302d6e25405cee0afb986
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2023-10-24 00:22:41 +0600

    Navbar added

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,12 +1,15 @@
-
-import { Outlet } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
+import NavBar from "../pages/shared/NavBar";
 
 const Root = () => {
-    return (
-        <div>
-            root<Outlet />
-        </div>
-    );
+  return (
+    <>
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-10">
+      <NavBar />
+      <Outlet />
+      </div>
+    </>
+  );
 };
 
 export default Root;
